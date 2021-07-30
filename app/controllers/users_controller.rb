9f3953da0b18735dbb6af01a6b1bbdd693fbf799
David Moina <davemolina777@gmail.com>
2021-07-30 10:57:13 -0500

    Added basic serializer information for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    render json: UserSerializer.new(@users)
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: @user.slice(:id, :username, :password, :email)
   end
 
   # POST /users
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render json: @user.slice(:id, :username, :password, :email)
     else
       render json: @user.errors, status: :unprocessable_entity
     end
