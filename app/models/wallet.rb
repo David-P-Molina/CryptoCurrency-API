39bde8b20a89ee22767bdc50d2aa4e7baa5d9968
David Moina <davemolina777@gmail.com>
2021-07-30 10:59:10 -0500

    Added username helper methods to wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,14 @@
 class Wallet < ApplicationRecord
   belongs_to :user
   has_many :coins, through :coin_wallets
+
+  #helpers
+
+  def username=(username)
+    self.username = User.find_or_create_by(username: username)
+  end
+
+  def username
+    self.user.try(:username)
+  end
 end
