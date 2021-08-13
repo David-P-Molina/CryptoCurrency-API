170dfdbe1bad3d907c8a34ea3b829a29bac7d596
David Moina <davemolina777@gmail.com>
2021-08-13 17:27:24 -0500

    fix formatting issue

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,8 +1,7 @@
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-key = "?CMC_PRO_API_KEY=fcde9892-d0e8-4f58-850c-6d58709fff97"
 limiter = "&id=1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,22&convert=USD"
-coins_response = JSON.parse(HTTParty.get("#{url + key + limiter}").body)
-coins = coins_response"data"]["1"]
+coins_response = JSON.parse(HTTParty.get("#{url + $key + limiter}").body)
+coins = coins_response["data"]
 
 
 # class Api
