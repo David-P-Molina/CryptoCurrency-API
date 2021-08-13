2a43230c173a4a7a48b3a2af1519a5a2455dafa6
David Moina <davemolina777@gmail.com>
2021-08-13 17:42:07 -0500

    update gem file

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,11 @@ module MemeCoinTradingSimulatorApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    Bundler.require(*Rails.groups)
 
+    Dotenv::Railtie.load
+    
+    HOSTNAME = ENV['HOSTNAME']
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
