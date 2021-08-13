fb34c03e76fbba8d0d33a9d9fff79e0995d6dc89
David Moina <davemolina777@gmail.com>
2021-08-13 14:50:16 -0500

    retrieve information for coins from 3rd party api

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,7 +1,9 @@
-url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
 key = "?CMC_PRO_API_KEY=fcde9892-d0e8-4f58-850c-6d58709fff97"
-limiter = "&start=1&limit=2&convert=USD"
-coins_response = HTTParty.get("#{url + key + limiter}")
+limiter = "&id=1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,22&convert=USD"
+coins_response = JSON.parse(HTTParty.get("#{url + key + limiter}").body)
+coins = coins_response"data"]["1"]
+
 
 # class Api
 #     include HTTParty
