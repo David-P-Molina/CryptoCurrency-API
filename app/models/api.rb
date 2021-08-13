bdc25927f123d2cf34aa6bd7e57a48b121f97436
David Moina <davemolina777@gmail.com>
2021-08-13 14:30:12 -0500

    Modify httparty request to recieve coin info

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,9 +1,12 @@
-require 'httparty'
+url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+key = "?CMC_PRO_API_KEY=fcde9892-d0e8-4f58-850c-6d58709fff97"
+limiter = "&start=1&limit=2&convert=USD"
+coins_response = HTTParty.get("#{url + key + limiter}")
 
-class Api
-    include HTTParty
-    base_uri = 'https://pro-api.coinmarketcap.com'
-    headers = {
-        CMC_PRO_API_KEY="+"fcde9892-d0e8-4f58-850c-6d58709fff97"
-    }
-end
+# class Api
+#     include HTTParty
+#     base_uri = 'https://pro-api.coinmarketcap.com'
+#     headers = {
+#         CMC_PRO_API_KEY="+"fcde9892-d0e8-4f58-850c-6d58709fff97"
+#     }
+# end
