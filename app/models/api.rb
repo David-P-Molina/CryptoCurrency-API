435814b8a8c1521f31d2843794da87f657bd8a6d
David Moina <davemolina777@gmail.com>
2021-08-16 11:04:20 -0500

    change assemble method into class method and pass to retrieve con method

--- app/models/api.rb
+++ app/models/api.rb
@@ -3,15 +3,15 @@ require 'httparty'
 
 class Api < ApplicationRecord
     include HTTParty
-    def assemble_url
+    def self.assemble_url
         api_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
         key = ENV[API_KEY]
         limiter = "&id=1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,22&convert=USD"
         api_call = "#{url + key + limiter}"
         api_call
     end
-    def self.retrieve_coins_info(url)
-        coins_response = HTTParty.get(url)
+    def self.retrieve_coins_info
+        coins_response = HTTParty.get(self.assemble_url)
         coins_list = coins_response.body
         coins_list
     end
