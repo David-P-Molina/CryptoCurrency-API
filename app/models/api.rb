cf8d79b6e403e873c7f77a781d39417c4937906b
David Moina <davemolina777@gmail.com>
2021-08-16 11:02:59 -0500

    Remove unnecessary code

--- app/models/api.rb
+++ app/models/api.rb
@@ -11,7 +11,7 @@ class Api < ApplicationRecord
         api_call
     end
     def self.retrieve_coins_info(url)
-        coins_response = HTTParty.get("#{url + key + limiter}")
+        coins_response = HTTParty.get(url)
         coins_list = coins_response.body
         coins_list
     end
@@ -23,17 +23,4 @@ class Api < ApplicationRecord
     def self.create_coins
 
     end
-url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-key = ENV[API_KEY]
-limiter = "&id=1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,22&convert=USD"
-coins = coins_response["data"]
-
 end
-
-# class Api
-#     include HTTParty
-#     base_uri = 'https://pro-api.coinmarketcap.com'
-#     headers = {
-#         CMC_PRO_API_KEY="+"fcde9892-d0e8-4f58-850c-6d58709fff97"
-#     }
-# end
