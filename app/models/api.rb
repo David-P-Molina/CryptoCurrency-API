80636b3b720975fd539a97d3b5d756edc614ed02
David Moina <davemolina777@gmail.com>
2021-08-15 21:15:24 -0500

    Add parse coin method

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,8 +1,11 @@
 class Api < ApplicationRecord
     def retrieve_coin_info
         coins_response = JSON.parse(HTTParty.get("#{url + key + limiter}").body)
+        coins_response
     end
-    
+    def parse_coin(json)
+        JSON.parse(json)
+    end
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
 key = ENV[API_KEY]
 limiter = "&id=1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,22&convert=USD"
