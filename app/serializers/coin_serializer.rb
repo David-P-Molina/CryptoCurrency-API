73d43a6332bbfda861aed3fe9d8a79c69803a954
David Moina <davemolina777@gmail.com>
2021-08-16 20:17:22 -0500

    Update serializer to include api_id

--- app/serializers/coin_serializer.rb
+++ app/serializers/coin_serializer.rb
@@ -1,4 +1,4 @@
 class CoinSerializer
   include JSONAPI::Serializer
-  attributes :name, :symbol, :initial_value, :day, :week, :marketcap, :volume_quantity, :volume_price, :circulation
+  attributes :name, :symbol, :initial_value, :day, :week, :marketcap, :volume_quantity, :volume_price, :circulation, :api_id
 end
