e60f6252888437ec0f7e404e371ae512b889f9de
David Moina <davemolina777@gmail.com>
2021-08-17 14:52:01 -0500

    Format user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  devise :database_authenticatable, :registerable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  devise :database_authenticatable, :registerable, :validatable,
+       :jwt_authenticatable, jwt_revocation_strategy: self
     has_one :wallet
     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
 end
